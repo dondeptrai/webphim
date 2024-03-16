e911a462c1c1b752b7ace5ac1dcc0f84ba0ebd9f
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-16 14:48:43 +0700

    Update user.php

--- view/user.php
+++ view/user.php
@@ -1,25 +1,22 @@
 <?php
-session_start();
-$link=new mysqli("localhost","root","","webphim");
-$sql1="SELECT * FROM `users` WHERE `name` LIKE '".$_SESSION['user_name']."'";
-
-$result = $link->query($sql1);
-$roww=$result->fetch_assoc();
+    session_start();
+    $link=new mysqli("localhost","root","","webphim");
+    $sql1="SELECT * FROM `users` WHERE `id` LIKE '".$_SESSION['user_id']."'";
+    $result = $link->query($sql1);
+    $roww=$result->fetch_assoc();
 
 
 if (!isset($_SESSION['user_name'])) {
     header('Location: view/login.php');
-    exit();
-}else{
+}else{ 
     ?>
         <div align="center"style="height:540px;padding-top:30px;color:white">
-            <h1>Hello: <?php echo $_SESSION['user_name']?></h1>
+            <h1>Hello: <?php echo $roww['name']?></h1>
             <h3><b>User name:</b> <?php echo $roww['name']?></h3>
-            <a href="view/logout.php" style="color:red;font-size: 1.3rem;"><b>Đăng xuất</a>
+            <a href="view/logout.php" style="color:red;font-size: 1.3rem;"><b>Đăng xuất</b></a>
             <br>
             <br>
             <a href="view/capnhat.php"style="color:gray;font-size: 1.1rem;"><b>Cập nhật thông tin</b></a>
-            <br>
             <br>
         </div>
     <?php
