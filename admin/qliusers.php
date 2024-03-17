49ed16a8c16cf740a8c196e4c21161f2dfea428c
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-17 20:16:11 +0700

    Update qliusers.php

--- admin/qliusers.php
+++ admin/qliusers.php
@@ -1,45 +1,59 @@
 <?php
-$link = new mysqli("localhost","root","","webphim");
-$sql="SELECT * FROM users";
-$result = mysqli_query($link,$sql);
-if($result === false) {
-        die("Lỗi truy vấn: " . mysqli_error($link));
+$link = new mysqli("localhost", "root", "", "webphim");
+
+if ($link->connect_error) {
+    die("Kết nối database thất bại: " . $link->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $link->query($sql);
+
+if ($result === false) {
+    die("Lỗi truy vấn: " . $link->error);
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM users WHERE id = $id";
+
+    if ($link->query($sql) === true) {
+        echo "<script>alert('User đã được xóa thành công '); history.back();</script>";
+    } else {
+        echo "Lỗi khi xóa user: " . $link->error;
     }
+}
 ?>
+
 <div align="center">
     <br>
     <div>
         <h1> Quản lý Users</h1>
-        <form action="" method="post">
-            <table align="center" border="1" style="width:1000px; padding: 8px; border-collapse: collapse;">
+        <table align="center" border="1" style="width:1000px; padding: 8px; border-collapse: collapse;">
             <tr style="background-color:gray" align="center">
                 <th>ID</th>
                 <th>Tên</th>
                 <th>Email</th>
                 <th>Password</th>
                 <th>User_type</th>
-                <th>delete</th>
+                <th>Delete</th>
             </tr>
-            <?php         
-            while($row=$result->fetch_assoc()){
+            <?php
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <tr align="center">
+                    <td><?php echo $row["id"] ?></td>
+                    <td><?php echo $row["name"] ?></td>
+                    <td><?php echo $row["email"] ?></td>
+                    <td><?php echo $row["password"] ?></td>
+                    <td><?php echo $row["user_type"] ?></td>
+                    <td>
+                        <a onclick="return confirm('Bạn có muốn xóa?')" 
+                           href="admin.php?pid=8&id=<?php echo $row['id'] ?>"style="color:white">Xóa</a>
+                    </td>
+                </tr>
+            <?php
+            }
             ?>
-            <tr align="center">
-                <td><?php echo $row["id"]?></td>
-                <td><?php echo $row["name"]?></td>
-                <td><?php echo $row["email"]?></td>
-                <td><?php echo $row["password"]?></td>
-                <?php
-            $phim=$row["user_type"];
-            $sql1="select * from users where user_type='user'";
-            $result1=$link->query($sql1);
-            $row1=$result1->fetch_assoc();
-            ?>
-                <td><?php echo $row1["user_type"]?></td>
-                <td><a onclick="return confirm('Bạn có muốn xóa?')" href="admin.php?pid=8&id=<?php echo $row['id']?>"style="color:white">Xóa</a></td>
-            </tr>
-            <?php         
-                }
-            ?>       
-            </table>
-</form>
+        </table>
+    </div>
 </div>
