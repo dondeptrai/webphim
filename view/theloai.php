e29f37c91b5e3876c40be7c88e38172357e7ef0d
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-23 01:34:19 +0700

    Update theloai.php

--- view/theloai.php
+++ view/theloai.php
@@ -1,27 +1,29 @@
 <?php 
     $link = new mysqli("localhost","root","","webphim");
     $sql = "SELECT * FROM theloai";
-    $resul=$link->query($sql);
+    $result_theloai = $link->query($sql);
 ?>
+
 <style>
     a:link{text-decoration:none;color:black;}
     a:hover{color:white}
 </style>
 
 <?php
-    while($row=$resul->fetch_assoc()){
+    while($row_theloai = $result_theloai->fetch_assoc()){
 ?>
     <div class="tag">
-        <a href="index.php?pid=1002&id=<?php echo $row["maTheloai"]?>"><?php echo $row["tenTheloai"] ?></a>
+        <a href="index.php?pid=1002&id=<?php echo $row_theloai["maTheloai"] ?>"><?php echo $row_theloai["tenTheloai"] ?></a>
     </div>
     
 <?php
     }
 ?>
+
 <div class="tag">
-    <a href="index.php?pid=1002">Phim Bộ</a>
+    <a href="index.php?pid=1014&Phan_Loai=Phim bộ">Phim Bộ</a>
 </div>
 
 <div class="tag">
-    <a href="index.php?pid=1002">Phim Lẻ</a>
+    <a href="index.php?pid=1014&Phan_Loai=Phim lẻ">Phim Lẻ</a>
 </div>
