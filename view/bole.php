c8f63dfba33290517e1d05c1571ca0050ded2d42
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-23 01:34:45 +0700

    Create bole.php

---
+++ view/bole.php
@@ -0,0 +1,31 @@
+<?php
+//kiểm tra phim lẻ hay bộ
+    $phan_loai = isset($_GET['Phan_Loai']) ? $_GET['Phan_Loai'] : '';
+    $link = new mysqli("localhost", "root", "", "webphim");
+
+    $phan_loai == 'Phim bộ' || $phan_loai == 'Phim lẻ';
+    $sql = "SELECT * FROM phim WHERE  Phan_Loai = '$phan_loai'";
+    $result = $link->query($sql);
+?>
+
+<div>
+    <?php
+        // Hiển thị danh sách phim
+        while ($row = $result->fetch_assoc()) {
+    ?>
+    <div class="main" style="float:left">
+        <div class="movie">
+            <a href="index.php?pid=1001&id=<?php echo $row['maPhim'] ?>" style="text-decoration:none;color:black;">
+                <img src="img/<?php echo $row['hinhanh'] ?>" style="width:220px;height:310px">
+                <br>
+                <div class="movie-info">
+                    <span><b><?php echo $row['Ten'] ?></b></span>
+                    <span class="green"><?php echo $row['Danh_Gia'] ?></span>
+                </div>
+                <span class="overview"><?php echo $row['Noi_Dung'] ?></span>
+            </a>
+        </div>
+    </div>
+    <?php
+        }
+    ?>
