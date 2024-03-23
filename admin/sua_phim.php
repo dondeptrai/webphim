04824f08ee5330cad7f3d5d0778502ea7029a445
mydayoanhh <163551598+mydayoanhh@users.noreply.github.com>
2024-03-23 13:38:54 +0700

    Update sua_phim.php

--- admin/sua_phim.php
+++ admin/sua_phim.php
@@ -47,11 +47,12 @@ $row = mysqli_fetch_assoc($query);
                     <label class ="c" for="quoc_gia">Quốc gia:</label>
                     <input type="text" id="quoc_gia" name="quoc_gia" value="<?php echo $row['Quoc_Gia']; ?>">
 
-                    <label class ="c" for="phan_loai">Phân loại:</label>
-                    <select id="phan_loai" name="phan_loai">
+                    <label class='c' for="phan_loai">Phân loại:</label>
+                    <select id="phan_loai" name="phan_loai" style="font-size: 1.2em; margin-bottom: 10px;margin-top: 10px;">
                     <option value="Phim bộ" <?php if($row['Phan_Loai'] == "Phim bộ") echo "selected"; ?>>Phim bộ</option>
                     <option value="Phim lẻ" <?php if($row['Phan_Loai'] == "Phim lẻ") echo "selected"; ?>>Phim lẻ</option>
-                    </select> <br>
+                    </select>
+                    <br>
 
                     <label class ="c" for="dien_vien">Diễn viên:</label>
                     <input type="text" id="dien_vien" name="dien_vien" value="<?php echo $row['Dien_Vien']; ?>">
