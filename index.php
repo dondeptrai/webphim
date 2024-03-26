3c1d8fa7f48dcb1d1855604be25e7e0f9df3ce27
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-26 15:24:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,69 +14,66 @@
         <?php include("view/logo.php"); ?>
         <div class="both">
             <?php include("view/search.php"); ?>
-            <?php include("view/user.php")?>
+            <?php include("view/user.php") ?>
         </div>
     </header>
-    <br>    
-    <div align="center">
-        <?php include("view/theloai.php"); ?>
-         <?php
-   if(!isset($_GET['pid'])) {
-    include("view/phimnoibat.php");
-   }
-  ?>
-    </div>
-
-    <?php
-    if(isset($_GET['pid'])) {
-        switch($_GET['pid']) {
-            case 1001:
-                include("view/view_page.php");
-                break;
-            case 1002:
-                include("view/movie.php");
-                break;
-            case 1003:
-                include("view/search_phim.php");
-                break;
-            case 1004:
-                include("view/hi.php");
-                break;
-            // case 1005:
-            //     include("view/login.php");
-            //     break;
-            case 1006:
-                include("view/logout.php");
-                break;
-            // case 1007:
-            //     include("view/register.php");
-            //     break;
-            case 1008:
-                include("view/user.php");
-                break;
-            case 1009:
+    <br>
+    <div >
+        <div class="" align="center">
+            <?php include("view/theloai.php"); ?>
+            <div align="center">
+            <?php
+                if (!isset($_GET['pid'])) {
+                    include("view/phimnoibat.php");
+                }
+            ?>
+            </div>
+            <?php
+            if (isset($_GET['pid'])) {
+                switch ($_GET['pid']) {
+                    case 1001:
+                        include("view/view_page.php");
+                        break;
+                    case 1002:
+                        include("view/movie.php");
+                        break;
+                    case 1003:
+                        include("view/search_phim.php");
+                        break;
+                    case 1004:
+                        include("view/hi.php");
+                        break;
+                    case 1006:
+                        include("view/logout.php");
+                        break;
+                    case 1008:
+                        include("view/user.php");
+                        break;
+                    case 1009:
+                        include("view/pagination.php");
+                        break;
+                    case 1010:
+                        include("view/watchmovie.php");
+                        break;
+                    case 1011:
+                        include("view/lichsuxemphim.php");
+                        break;
+                    case 1012:
+                        include("view/capnhat.php");
+                        break;
+                    case 1013:
+                        include("view/doipass.php");
+                        break;
+                    case 1014:
+                        include("view/bole.php");
+                        break;
+                }
+            } else {
                 include("view/pagination.php");
-                break;
-            case 1010:
-                include("view/watchmovie.php");
-                break;
-            case 1011:
-                include("view/lichsuxemphim.php");
-                break;
-            case 1012:
-                include("view/capnhat.php");
-                break;
-            case 1013:
-                include("view/doipass.php");
-                break;
-            case 1014:
-                include("view/bole.php");
-                break;
-        }
-    } else {
-        include("view/pagination.php");
-    }
-    ?>
-    <script src="script.js"></script>
+            }
+            ?>
+        </div>
+        
+    </div>
 </body>
 </html>
