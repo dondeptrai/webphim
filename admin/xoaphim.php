42780b56504a6769fb27eb13f02e8f18297d993b
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-26 15:18:51 +0700

    Update xoaphim.php

--- admin/xoaphim.php
+++ admin/xoaphim.php
@@ -18,11 +18,11 @@
             exit(); // Kết thúc script sau khi chuyển hướng
         } else {
             // Nếu xoá không thành công, hiển thị thông báo và quay lại trang trước đó
-            echo "<script>alert('Delete không thành công, kiểm tra lại! '); history.back();</script>";
+            echo "<script>alert('Xóa không thành công kiểm tra lại.'); window.location.href='admin.php?pid=2'</script>";    
         }
     } else {
         // Nếu không có tham số 'id' được truyền qua URL
-        echo "<script>alert('Không có id được truyền qua URL! '); history.back();</script>";
+        echo "<script>alert('Không có id được truyền vào URL'); window.location.href='admin.php?pid=2'</script>";    
     }
 
     // Đóng kết nối đến cơ sở dữ liệu
