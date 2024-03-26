e6fc5cbd566159414a92821657594e117e9a5a9b
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-03-26 15:16:12 +0700

    Update ctrl_themphim.php

--- admin/ctrl_themphim.php
+++ admin/ctrl_themphim.php
@@ -15,17 +15,16 @@
     $dienvien=$_POST["Dien_Vien"];
     $nam=$_POST["Nam"];
     $thoiluong=$_POST["Thoi_Luong"];
-    $danhgia=$_POST["Danh_Gia"];
     $thongtin = $_POST["Noi_Dung"];
     $ngonngu=$_POST["Ngon_Ngu"];
     $hinhanh = $_FILES["hinhanh"]["name"];
     $trailer=$_POST["trailerlink"];
     $linkphim = $_POST["linkphim"];
-    $themsp_qr = "INSERT INTO phim (maPhim,maTheloai,Ten,Quoc_Gia,Phan_Loai,Dien_Vien,Nam,Thoi_Luong,Danh_Gia,Noi_Dung,Ngon_Ngu,hinhanh,trailerlink,linkphim) VALUES ('$id','$maLP','$ten', '$quocgia', '$phanloai','$dienvien','$nam','$thoiluong','$danhgia','$thongtin','$ngonngu','$hinhanh','$trailer','$linkphim')";
+    $themsp_qr = "INSERT INTO phim (maPhim,maTheloai,Ten,Quoc_Gia,Phan_Loai,Dien_Vien,Nam,Thoi_Luong,Noi_Dung,Ngon_Ngu,hinhanh,trailerlink,linkphim) VALUES ('$id','$maLP','$ten', '$quocgia', '$phanloai','$dienvien','$nam','$thoiluong','$thongtin','$ngonngu','$hinhanh','$trailer','$linkphim')";
 
     if ($link->query($themsp_qr)){
         echo "<script>alert('Thêm phim thành công'); window.location.href='admin.php?pid=2';</script>";
     } else {
-        echo "<script>alert('Thêm không thành công, kiểm tra lại! Lỗi: '); history.back();</script>";
+        echo "<script>alert('Thêm không thành công.Kiểm tra lại lỗi!'); window.location.href='admin.php?pid=2'</script>";    
     }
 ?>
